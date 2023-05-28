7e5f7e12960dae3a3f0a83d109cfa5dbf7a98e70
bestrostik <134850962+bestrostik@users.noreply.github.com>
2023-05-28 16:44:21 +0300

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -1,16 +1,19 @@
 #1 
-from pygame import *
 
-class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y,size_x, size_y, player_speed):
-        super().__init__()
-
-        self.image = transform.scale(image.load(player_image), (size_x, size_y))
-        self.speed = player_speed
-        self.rect = self.image.get_rect()
-        self.rect.x = player_x
-        self.rect.y = player_y
-    def reset(self):
-        window.blit(self.image, (self.rect.x, self.rect.y))
 
 #2
+
+#3
+while game:
+    for e in event.get():
+        if e.type = QUIT:
+        game = False 
+    if finish != True:
+        window.fill(back)
+        racket1.update_1()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed y
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect (racket2, ball):
+            speed_x *= 1
+            speed_y *= 1
