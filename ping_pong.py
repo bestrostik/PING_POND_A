2a1a9c15e2d52114e30f5572bbff019240c15917
bestrostik <134850962+bestrostik@users.noreply.github.com>
2023-05-28 16:45:30 +0300

    Update ping_pong.py
    1

--- ping_pong.py
+++ ping_pong.py
@@ -14,3 +14,18 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 #2
+
+#3
+while game:
+    for e in event.get():
+        if e.type = QUIT:
+        game = False 
+    if finish != True:
+        window.fill(back)
+        racket1.update_1()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed y
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect (racket2, ball):
+            speed_x *= 1
+            speed_y *= 1
